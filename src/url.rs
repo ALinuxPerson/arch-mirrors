c3ec364dc4a60ed135600805d130b2e6270d2ee8
ALinuxPerson <alinuxperson@gmail.com>
2021-05-20 11:20:15 +0800

    Add documentation for url.rs

--- src/url.rs
+++ src/url.rs
@@ -1,6 +1,8 @@
+//! This is where the [`Url`] struct and all of its dependencies go.
 use crate::Country;
 use serde::{Deserialize, Serialize};
 
+/// Raw, typed form of the JSON output of each url listed in [`country::Raw`](crate::country::Raw).
 #[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct Raw {
     url: String,
@@ -19,20 +21,47 @@ pub(crate) struct Raw {
     details: String,
 }
 
+/// An Arch Linux mirror and its statistics.
 #[derive(Debug, Clone, PartialOrd, PartialEq)]
 pub struct Url {
+    /// The url of the mirror.
     pub url: url::Url,
+
+    /// The protocol that this mirror uses.
     pub protocol: crate::Protocol,
+
+    /// The last time it synced from Arch Linux server.
     pub last_sync: Option<chrono::DateTime<chrono::Utc>>,
+
+    /// Completion PCT. Unknown what this means.
     pub completion_pct: f64,
+
+    /// The average duration. Unknown what this means.
     pub duration_average: Option<f64>,
+
+    /// Duration StdDev. Unknown what this means.
     pub duration_stddev: Option<f64>,
+
+    /// The score of the mirror. This is currently calculated as `(hours delay + average duration + standard deviation) / completion percentage`.
+    /// Lower is better.
     pub score: Option<f64>,
+
+    /// Whether or not the mirror is active.
     pub active: bool,
+
+    /// The country where the mirror resides in.
     pub country: crate::Country,
+
+    /// Whether or not this mirror has Arch Linux ISOs(?)
     pub isos: bool,
+
+    /// Whether or not this mirror supports IPv4.
     pub ipv4: bool,
+
+    /// Whether or not this mirror supports IPv6.
     pub ipv6: bool,
+
+    /// The details of the mirror.
     pub details: String,
 }
 
