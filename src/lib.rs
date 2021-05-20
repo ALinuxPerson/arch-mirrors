d80ead9e557b502d24e73a9edade3a27e3e7a98b
ALinuxPerson <alinuxperson@gmail.com>
2021-05-20 09:35:10 +0800

    Add documentation for lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+//! # Arch Mirrors
+//! Get and parse the Arch Linux mirrors.
 #![warn(missing_docs)]
 #![warn(rustdoc::missing_crate_level_docs)]
 #![warn(invalid_codeblock_attributes)]
@@ -12,6 +14,8 @@ pub use country::Country;
 pub use protocol::Protocol;
 pub use status::Status;
 
+/// Shorthand for [`Status::get()`](Status::get). This gets the mirror status of all Arch Linux
+/// mirrors.
 pub async fn get_status() -> reqwest::Result<Status> {
     Status::get().await
 }
