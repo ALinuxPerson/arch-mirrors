cee98191417406b4bfe5373701a82456c3e4d0d8
ALinuxPerson <alinuxperson@gmail.com>
2021-05-20 09:51:51 +0800

    Add ability to create documentation

--- src/macros.rs
+++ src/macros.rs
@@ -1,5 +1,17 @@
 macro_rules! countries {
-    ($name:literal ($snake_case:ident): $kind:ident => $code:ident) => {
-        
+    ($($(#[$docs:meta])* $name:literal ($snake_case:ident): $kind:ident => $code:ident),+) => {
+        #[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
+        pub enum Kind {
+            $($(#[$docs])* $kind),+,
+
+            /// Any unsupported country.
+            Other(String)
+        }
     };
+}
+
+countries! {
+    /// Australia.
+    "Australia" (AUSTRALIA): Australia => AU,
+    "Germany" (GERMANY): Germany => DE
 }
